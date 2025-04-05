140f1b8be9647713ffc4f70a07104ec052d8246f
Div Surya <divyansckashyap@gmail.com>
2025-04-05 16:47:29 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version = '0.0.0',
+    author = 'Divyans C Kashyap',
+    author_email = 'divyansckashyap@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
+
+
+
